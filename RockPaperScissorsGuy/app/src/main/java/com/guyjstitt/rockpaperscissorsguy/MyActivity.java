fbc9ad4272be8f3a3ee94186448576a713862238
Guy Stitt <guy.stitt@cbsinteractive.com>
2014-09-20 19:53:56 -0400

    removed random 1 from initialize

--- RockPaperScissorsGuy/app/src/main/java/com/guyjstitt/rockpaperscissorsguy/MyActivity.java
+++ RockPaperScissorsGuy/app/src/main/java/com/guyjstitt/rockpaperscissorsguy/MyActivity.java
@@ -41,7 +41,7 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-        Initial1ize();
+        Initialize();
     }
 
     private void Initialize() {
